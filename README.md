b9608e118d79879dde8ece2e8ef7488bed9bfff4
jayashibus <jayashibu@gmail.com>
2023-05-04 21:28:42 +0530

    Readme file added

--- README.md
+++ README.md
@@ -22,7 +22,43 @@ generate_otp_email function that takes one argument (the email address to send t
 generate_OTP_email("tester1@dso.org.sg")
 ```
 
-check_OTP function that takes an object containing the email ID and User entering OTP value:
+check_OTP function that takes an Input stream object containing the email ID and User entering OTP value:
+
+```sh
+userDetails = { user_email: "tester1@dso.org.sg", otp: "173923" };
+```
+
+```sh
+check_OTP(userDetails)
+```
+
+## Test
+
+### Valid Email
+
+Email Ends with @dso.org.sg are Valid
+
+```sh
+tester1@dso.org.sg,
+admin.hr@dso.org.sg,
+```
+
+### Invalid Email
+
+Email Ends with @dso.org.sg are Valid
+
+```sh
+tester2@dso.org.sg.yahoo.com,
+tester2@yahoo.com,
+```
+
+### Assume Valid OTP
+
+```sh
+173923
+```
+
+You can pass the valid OTP in below format for testing
 
 ```sh
 userDetails = { user_email: "tester1@dso.org.sg", otp: "173923" };
--- index.js
+++ index.js
@@ -6,31 +6,20 @@ function generate_OTP_email(user_email) {
     return message.CONSTANTS.statusMsg.STATUS_EMAIL_INVALID;
   }
 
-  // Validate email address for valid chars, etc.
+  // Validate email address.
   const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
   if (!regex.test(user_email)) {
     return message.CONSTANTS.statusMsg.STATUS_EMAIL_INVALID;
   }
 
-  // Generate a random 6-digit OTP code
   const otp = Math.floor(Math.random() * 900000) + 100000;
   const start_time = new Date().getTime();
 
-  // Store the OTP code in the database with the user's email address
-
-  // Check if OTP is valid
-  function isValidOTP(otp) {
-    // implement OTP validation logic here
-  }
-  // ...
-
-  // Use an email service provider to send the OTP code to the user's email address
   const email_body = `Your OTP Code is ${otp}. The code is valid for 1 minute.`;
   if (!send_email(user_email, email_body)) {
     return message.CONSTANTS.statusMsg.STATUS_EMAIL_FAIL;
   }
 
-  // Return status code
   return message.CONSTANTS.statusMsg.STATUS_EMAIL_OK;
 }
 
@@ -38,36 +27,23 @@ function check_OTP(input) {
   let otp = input.otp;
   let tries = 0;
   const start_time = new Date().getTime();
-  const timeout = 60000; // 1 minute timeout
+  const timeout = 60000;
+
+  // const timeout = setTimeout(() => {
+  //   return message.CONSTANTS.statusMsg.STATUS_OTP_TIMEOUT;
+  // }, 10000);
 
   if (tries < 10) {
-    // Check for timeout
     const elapsed_time = new Date().getTime() - start_time;
     if (elapsed_time >= timeout) {
       return message.CONSTANTS.statusMsg.STATUS_OTP_TIMEOUT;
     }
 
-    // Wait for user input
-    // try {
-    //   otp = await input.readOTP(timeout - elapsed_time);
-    // } catch (error) {
-    //   // Handle timeout or IO error
-    //   return STATUS_OTP_TIMEOUT;
-    // }
-
-    // Validate OTP code
-    // Retrieve the stored OTP code from the database with the user's email address
-    // ...
-
-    //stored_otp = stored_otp(input.email_address);
-
     stored_otp = "173923";
 
-    if (otp === stored_otp) {
-      // OTP is valid
+    if (validateOTP(otp) && otp === stored_otp) {
       return message.CONSTANTS.statusMsg.STATUS_OTP_OK;
     } else {
-      // OTP is invalid
       tries++;
       const remaining_tries = 10 - tries;
       if (remaining_tries === 0) {
@@ -81,10 +57,16 @@ function check_OTP(input) {
   return message.CONSTANTS.statusMsg.STATUS_OTP_FAIL;
 }
 
-// Send email to the user with the OTP code
-function send_email(email_address, email_body) {
-  // Implement a function to send email to the user's email address with the OTP code
+function storeOTP(email, otp) {
+  // implement OTP storage logic here
+}
 
+function validateOTP(otp) {
+  const re = /^[0-9]{6}$/;
+  return re.test(otp);
+}
+
+function send_email(email_address, email_body) {
   console.log(email_address);
   console.log(email_body);
 
