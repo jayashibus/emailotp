b4e41435fc64d50007a804902f90ce2a2fc747b2
jayashibus <jayashibu@gmail.com>
2023-05-11 18:38:33 +0530

    Email and OTP improved

--- index.js
+++ index.js
@@ -1,18 +1,25 @@
 const message = require("./constants");
 
 function generate_OTP_email(user_email) {
-  // Validate email domain
-  if (!user_email.endsWith("@dso.org.sg")) {
-    return message.CONSTANTS.statusMsg.STATUS_EMAIL_INVALID;
-  }
+  // // Validate email domain
+  // if (!user_email.endsWith("@dso.org.sg")) {
+  //   return message.CONSTANTS.statusMsg.STATUS_EMAIL_INVALID;
+  // }
 
-  // Validate email address.
-  const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  //
+  // const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  // if (!regex.test(user_email)) {
+  //   return message.CONSTANTS.statusMsg.STATUS_EMAIL_INVALID;
+  // }
+
+  // Validate email address and Validate email domain and check special charactor
+
+  const regex = /^[a-zA-Z0-9._%+-]+@(dso\.org\.sg)$/i;
   if (!regex.test(user_email)) {
     return message.CONSTANTS.statusMsg.STATUS_EMAIL_INVALID;
   }
 
-  const otp = Math.floor(Math.random() * 900000) + 100000;
+  const otp = String(Math.floor(Math.random() * 1000000)).padStart(6, "0");
   const start_time = new Date().getTime();
 
   const email_body = `Your OTP Code is ${otp}. The code is valid for 1 minute.`;
@@ -25,9 +32,12 @@ function generate_OTP_email(user_email) {
 
 function check_OTP(input) {
   let otp = input.otp;
+
+  //Tries should store in the database and fetch of every OTP entry
   let tries = 0;
   const start_time = new Date().getTime();
   const timeout = 60000;
+  // setTimeout(timerCallback, 60000);
 
   // const timeout = setTimeout(() => {
   //   return message.CONSTANTS.statusMsg.STATUS_OTP_TIMEOUT;
@@ -39,6 +49,7 @@ function check_OTP(input) {
       return message.CONSTANTS.statusMsg.STATUS_OTP_TIMEOUT;
     }
 
+    //Hardcoded stored OTP. Its suppose to fetch from DB
     stored_otp = "173923";
 
     if (validateOTP(otp) && otp === stored_otp) {
@@ -61,10 +72,19 @@ function storeOTP(email, otp) {
   // implement OTP storage logic here
 }
 
+function getOTP(email) {
+  // implement get OTP  logic here
+}
+
 function validateOTP(otp) {
   const re = /^[0-9]{6}$/;
   return re.test(otp);
 }
+
+// function timerCallback() {
+//   console.log("1 minute has passed!");
+//   return message.CONSTANTS.statusMsg.STATUS_OTP_TIMEOUT;
+// }
 
 function send_email(email_address, email_body) {
   console.log(email_address);
